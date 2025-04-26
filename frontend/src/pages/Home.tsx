5f75be8ddad14e7b00dc5c84046c768d954a8a4c
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-04-26 21:36:43 +0530

    Update movies 02

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -272,7 +272,7 @@ const Home: React.FC = () => {
       {Websites.length > 0 && (
         <div className="mt-8">
           <h2 className="text-2xl font-bold text-gray-900 mb-4">
-            Hosted websites
+            Students sites 🌐
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
